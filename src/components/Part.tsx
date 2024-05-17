de15ed0407c6566f255f5417ffc94cddb4c1940b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-17 21:08:45 +0300

    adding exhaustive type checing on default switch statement to handle cases properly

--- src/components/Part.tsx
+++ src/components/Part.tsx
@@ -2,6 +2,10 @@ import Content from "./Content";
 import { CoursePart } from "../types";
 
 const Part=({courseParts}:{courseParts:CoursePart[]})=>{
+  const assertNever=(value:never):never=>{
+    throw new Error(`unhandled discriminated union member: ${JSON.stringify(value)}`)
+
+  };
   return(<div> {
     //discriminated union technique  for type narrowing
     courseParts.map((part)=>{
@@ -14,8 +18,9 @@ const Part=({courseParts}:{courseParts:CoursePart[]})=>{
         return <Content key={part.name} part={part}/>
       case 'special':
         return <Content key={part.name} part={part}/>
+      //exhautive typechecking for union type data
       default:
-        return;
+        assertNever(part);
    }
   })}
     </div>)
