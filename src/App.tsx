03f997fc7b8366da21ca3e8f6c49c0f0147a7349
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-17 20:53:00 +0300

    declaring component needed for app component Header,Part,Total and using it to render course data

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,16 @@
-interface HeaderProps{
-  name:string;
-}
-
-interface Course{
-  name:string;
-  exerciseCount:number
-}
-
-interface ContentProps{
-  courseParts:Course[]
-}
-
-const Header=(props:HeaderProps):JSX.Element=>{
- return <p>{props.name}</p>
-}
-
-const Content=({ courseParts }:ContentProps):JSX.Element=>{
-  return(<div> <p>
-   {courseParts[0].name} {courseParts[0].exerciseCount}
- </p>
- <p>
-   {courseParts[1].name} {courseParts[1].exerciseCount}
- </p>
- <p>
-   {courseParts[2].name} {courseParts[2].exerciseCount}
- </p></div>)
-}
-
-const Total=({courseParts}:ContentProps)=>{
-  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
-  return (<div>
-       <p>
-       Number of exercises {totalExercises}
-     </p>
-  </div>)
-}
+import Header from "./components/Header";
+import Part from "./components/Part";
+import Total from "./components/Total";
+import { courseParts } from "./types";
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
-
+ 
+  
   return (
     <div>
      <Header name={courseName}/>
-     <Content courseParts={courseParts}/>
+     <Part courseParts={courseParts}/>
      <Total courseParts={courseParts}/>
     </div>
   );
